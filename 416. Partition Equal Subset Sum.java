52181ff57ecb6ca773da8104a126c317a3dc1421
miao yang <miao.yang10@t-mobile.com>
2020-10-18 15:22:42 -0700

    101820 solved

---
+++ 416. Partition Equal Subset Sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum /= 2;
+        
+        boolean[] dp = new boolean[sum + 1]; // 是否满足元素和为i
+        dp[0] = true;
+        
+        for (int num : nums) {
+            for (int i = sum; i >= num; i--) {
+                dp[i] = dp[i] || dp[i - num]; // 选或者不选当前的num
+            }
+        }
+        return dp[sum];
+    }
+}
